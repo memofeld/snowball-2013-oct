7c8e38fd6c6d2d0ed0b37c1a245dcd3120f51273
Juergen Weismueller <jweismueller@prodyna.com>
2013-10-14 14:30:49 +0200

    Update

---
+++ goodbye/src/main/java/com/prodyna/academy/greeting/util/StringResourceIterable.java
@@ -0,0 +1,19 @@
+package com.prodyna.academy.greeting.util;
+
+import java.util.Iterator;
+
+public class StringResourceIterable implements Iterable<String>, AutoCloseable {
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
