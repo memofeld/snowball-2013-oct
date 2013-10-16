e745a889b2457cfa946eb40fee80e1e786b8b569
Juergen Weismueller <jweismueller@prodyna.com>
2013-10-16 15:38:33 +0200

    Update

--- goodbye/src/main/java/com/prodyna/academy/greeting/mbean/Performance.java
+++ goodbye/src/main/java/com/prodyna/academy/greeting/mbean/Performance.java
@@ -1,5 +1,25 @@
 package com.prodyna.academy.greeting.mbean;
 
+import java.util.List;
+
 public class Performance implements PerformanceMBean {
+
+	@Override
+	public void report(String service, String method, long time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<String> listAvg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- goodbye/src/main/java/com/prodyna/academy/greeting/mbean/PerformanceMBean.java
+++ goodbye/src/main/java/com/prodyna/academy/greeting/mbean/PerformanceMBean.java
@@ -1,5 +1,13 @@
 package com.prodyna.academy.greeting.mbean;
 
+import java.util.List;
+
 public interface PerformanceMBean {
+
+	void report(String service, String method, long time);
+
+	void reset();
+
+	List<String> listAvg();
 
 }
