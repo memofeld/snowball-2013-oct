8442f14b407160d90557c75401b1d02f5885b586
Juergen Weismueller <jweismueller@prodyna.com>
2013-10-16 22:03:58 +0200

    Geecon

--- geecon/ejb/src/main/java/com/prodyna/academy/geecon/domain/Location.java
+++ geecon/ejb/src/main/java/com/prodyna/academy/geecon/domain/Location.java
@@ -1,7 +1,12 @@
 package com.prodyna.academy.geecon.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Location extends BaseEntity {
@@ -10,6 +15,13 @@ public class Location extends BaseEntity {
 	private String country;
 
 	@Column(length = 64)
+	private String locality;
+
+	@Column(length = 64)
 	private String city;
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "location_id")
+	private List<Room> roomList;
 
 }
