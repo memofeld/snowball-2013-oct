588f3844985150d0fee0cb12df17ae9ba512ed20
mgajic <milangajic91@gmail.com>
2013-10-17 12:16:39 +0200

    geecon

--- geecon/ejb/src/main/java/com/prodyna/academy/geecon/service/ConferenceServiceBean.java
+++ geecon/ejb/src/main/java/com/prodyna/academy/geecon/service/ConferenceServiceBean.java
@@ -1,15 +1,40 @@
 package com.prodyna.academy.geecon.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
 
 import com.prodyna.academy.geecon.domain.Conference;
 import com.prodyna.academy.geecon.util.CalendarUtil;
 
 @Stateless
 public class ConferenceServiceBean {
+
+	@Inject
+	private EntityManager em;
+
+	@Inject
+	private Logger logger;
+
+	public Conference createConference(String name, Calendar dateFrom, Calendar dateTill) {
+
+		Conference entry = new Conference();
+		entry.setDateFrom(CalendarUtil.getCalendar(2013, 10, 15));
+		entry.setName(name);
+		entry.setDateTill(CalendarUtil.getCalendar(2013, 10, 17));
+		logger.info("Added a new name: " + name);
+		em.persist(entry);
+
+		return entry;
+
+	}
 
 	public List<Conference> getConferenceList() {
 		// TODO read from database
@@ -31,6 +56,11 @@ public class ConferenceServiceBean {
 		}
 		return out;
 
+	}
+
+	public List<Conference> getEntries() {
+		TypedQuery<Conference> query = em.createQuery("SELECT C FROM Conference C", Conference.class);
+		return query.getResultList();
 	}
 
 }
--- geecon/ejb/src/test/java/com/prodyna/academy/geecon/ConferenceServiceTest.java
+++ geecon/ejb/src/test/java/com/prodyna/academy/geecon/ConferenceServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 
 import com.prodyna.academy.geecon.domain.Conference;
 import com.prodyna.academy.geecon.service.ConferenceServiceBean;
+import com.prodyna.academy.geecon.util.CalendarUtil;
 
 @RunWith(Arquillian.class)
 public class ConferenceServiceTest extends AbstractTest {
@@ -27,5 +28,19 @@ public class ConferenceServiceTest extends AbstractTest {
 				found = true;
 		}
 		Assert.assertTrue(found);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		conferenceService.createConference("bilo sta", CalendarUtil.getCalendar(2013, 10, 15),
+				CalendarUtil.getCalendar(2013, 10, 17));
+
+	}
+
+	@Test
+	public void test2() throws Exception {
+
+		// conferenceService.getEntries();
+		List<Conference> conf = conferenceService.getEntries();
 	}
 }
